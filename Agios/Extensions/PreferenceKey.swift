89385cbacf3935ac552c06cd500b84e7622e54de
zuriks <victorander18@gmail.com>
2024-08-02 00:32:52 +0100

    [Clean]

---
+++ Agios/Extensions/PreferenceKey.swift
@@ -0,0 +1,46 @@
+
+//
+//  ScrollViewPreferenceKey.swift
+//  Agios
+//
+//  Created by Victor on 8/1/24.
+//
+
+import SwiftUI
+
+
+struct ScrollViewOffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+    
+}
+
+extension View {
+    func onScrollViewOffsetChanged(action: @escaping (_ offset: CGFloat) -> ()) -> some View {
+        self.background(
+                GeometryReader { geo in
+                    Text("")
+                        .preference(key: ScrollViewOffsetPreferenceKey.self, value: geo.frame(in: .global).minY)
+                }
+            )
+            .onPreferenceChange(ScrollViewOffsetPreferenceKey.self, perform: { value in
+                action(value)
+            })
+    }
+}
+
+
+struct BackgroundBlurView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
